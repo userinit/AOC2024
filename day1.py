d3d2baea5c534cc81e4d9e950aa9101d5e50e68c
userinit <93477626+userinit@users.noreply.github.com>
2024-12-02 21:55:05 +0000

    Completed day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,16 @@
+list1 = []
+list2 = []
+distanceSum = 0
 
+with open("day1-input.txt","r") as twoLists:
+    for line in twoLists:
+        lineArray = line.split("   ") # splits line into two elements
+        list1.append(int(lineArray[0]))
+        list2.append(int(lineArray[1].strip()))
+list1.sort()
+list2.sort()
+
+for i in range(len(list1)):
+    distance = list1[i] - list2[i]
+    distanceSum += abs(distance)
+print(distanceSum)
