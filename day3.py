30a763c30cc9e4df04350e28684e61728b4074e0
userinit <93477626+userinit@users.noreply.github.com>
2024-12-09 21:55:33 +0000

    Completed day 3 task 2

--- day3.py
+++ day3.py
@@ -1,5 +1,6 @@
 import re #For RegEx
 
+# Task 1
 total = 0
 regex = r"mul\(\d+,\d+\)"
 
@@ -12,4 +13,25 @@ with open("day3-input.txt","r") as file:
             num1 = int(splitItemArr[0])
             num2 = int(splitItemArr[1])
             total += num1 * num2
+print(total)
+
+# Task 2
+total = 0
+regex = r"do\(\)|don't\(\)|mul\(\d+,\d+\)"
+mulActivated = True
+
+with open("day3-input.txt","r") as file:
+    for line in file:
+        regexArr = re.findall(regex, line)
+        for item in regexArr:
+            if item[0:3] == "mul" and mulActivated:
+                strippedItem = re.sub(r"[^0-9,]", "", item)
+                splitItemArr = strippedItem.split(",")
+                num1 = int(splitItemArr[0])
+                num2 = int(splitItemArr[1])
+                total += num1 * num2
+            elif item[0:4] == "do()":
+                mulActivated = True
+            elif item[0:7] == "don't()":
+                mulActivated = False
 print(total)
