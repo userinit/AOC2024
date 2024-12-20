afcf2a343ac3060e1a8085e9949be81160d7695a
userinit <93477626+userinit@users.noreply.github.com>
2024-12-20 18:07:24 +0000

    Completed day 5 task 2

--- day5.py
+++ day5.py
@@ -1,9 +1,9 @@
 import re
 
+# Part One
 rules = []
 content = []
 middleNumberTotal = 0
-
 
 with open("day5-input.txt","r") as file:
     for line in file: # adding rules
@@ -34,4 +34,45 @@ for update in content:
         middleIndex = int((len(updateArr)-1)/2)
         middleNumber = updateArr[middleIndex]
         middleNumberTotal += middleNumber
-print(middleNumberTotal)
+print(f"The sum of the values of the middle numbers of the valid rules is {middleNumberTotal}.")
+
+# Part Two
+middleNumberTotal = 0
+
+for update in content:
+    validUpdate = True
+    strUpdateArr = update.split(",")
+    updateArr = [int(item) for item in strUpdateArr]
+    for rule in rules:
+        if not validUpdate:
+            break
+        ruleArray = rule.split("|")
+        ruleNumOne = int(ruleArray[0])
+        ruleNumTwo = int(ruleArray[1])
+        if ruleNumOne in updateArr and ruleNumTwo in updateArr:
+            ruleOneIndex = updateArr.index(ruleNumOne)
+            ruleTwoIndex = updateArr.index(ruleNumTwo)
+            if ruleOneIndex > ruleTwoIndex:
+                validUpdate = False
+    if not validUpdate:
+        while True:
+            changed = False
+            for rule in rules:
+                ruleArray = rule.split("|")
+                ruleNumOne = int(ruleArray[0])
+                ruleNumTwo = int(ruleArray[1])
+                if ruleNumOne in updateArr and ruleNumTwo in updateArr:
+                    ruleOneIndex = updateArr.index(ruleNumOne)
+                    ruleTwoIndex = updateArr.index(ruleNumTwo)
+                    if ruleOneIndex > ruleTwoIndex:
+                        changed = True
+                        updateArr[ruleOneIndex] = ruleNumTwo
+                        updateArr[ruleTwoIndex] = ruleNumOne
+                        # Disclaimer: This algorithm prioritizes simplicity over efficiency.
+                        # The time complexity is high (O(m * n^2 * k)), but the implementation is straightforward.
+            if not changed:
+                break
+        middleIndex = int((len(updateArr)-1)/2)
+        middleNumber = updateArr[middleIndex]
+        middleNumberTotal += middleNumber
+print(f"The sum of the values of the middle numbers of the invalid rules is {middleNumberTotal}.")
