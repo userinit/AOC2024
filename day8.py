c5ab8a958863a1ae20bedbf24c3bb20b11d23fde
userinit <93477626+userinit@users.noreply.github.com>
2024-12-24 15:32:01 +0000

    Completed day 8 task 2

--- day8.py
+++ day8.py
@@ -5,6 +5,7 @@ frequencies = []
 validAntinodes = 0
 antinodeCoordinates = set()
 
+# Part One
 with open("day8-input.txt","r") as file:
     for line in file:
         line = line.strip()
@@ -20,8 +21,8 @@ for row in matrix:
             frequencies.append(item)
 for item in frequencies:
     antennaPositions = []
-    for y,row in enumerate(matrix):
-        for x,value in enumerate(row):
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
             if value == item:
                 antennaPositions.append((x,y))
     for i in range(len(antennaPositions)):
@@ -38,5 +39,48 @@ for item in frequencies:
             if 0 <= pointAfter[0] <= len(matrix) - 1 and 0 <= pointAfter[1] <= len(matrix[0]) - 1 and pointAfter not in antinodeCoordinates:
                 validAntinodes += 1
                 antinodeCoordinates.add(pointAfter)
-print(validAntinodes)
+print(f"The amount of antinodes with limited distance is {validAntinodes}")
 
+# Part Two
+antinodeCoordinates = set()
+validAntinodes = 0
+
+for item in frequencies:
+    antennaPositions = []
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
+            if value == item:
+                antennaPositions.append((x,y))
+    for i in range(len(antennaPositions)):
+        for j in range(i+1,len(antennaPositions)):
+            a = antennaPositions[i] # first point
+            b = antennaPositions[j] # second point
+            dx = b[0]-a[0]
+            dy = b[1]-a[1]
+            if a not in antinodeCoordinates:
+                antinodeCoordinates.add(a)
+                validAntinodes += 1
+            if b not in antinodeCoordinates:
+                antinodeCoordinates.add(b)
+                validAntinodes += 1
+            currenta = a
+            currentb = b
+            while True:
+                nexta = (currenta[0]-dx,currenta[1]-dy)
+                if 0 <= nexta[0] <= len(matrix) - 1 and 0 <= nexta[1] <= len(matrix[0]) - 1:
+                    if nexta not in antinodeCoordinates:
+                        validAntinodes += 1
+                        antinodeCoordinates.add(nexta)
+                    currenta = nexta
+                else:
+                    break
+            while True:
+                nextb = (currentb[0]+dx,currentb[1]+dy)
+                if 0 <= nextb[0] <= len(matrix) - 1 and 0 <= nextb[1] <= len(matrix[0]) - 1:
+                    if nextb not in antinodeCoordinates:
+                        validAntinodes += 1
+                        antinodeCoordinates.add(nextb)
+                    currentb = nextb
+                else:
+                    break
+print(f"The amount of antinodes with infinite distance is {validAntinodes}")
